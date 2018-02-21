68c59de6e192552e1bb707a22cfe9f93178dbb07
Jonathan Perea <jtperea@rams.colostate.edu>
2018-02-21 10:29:35 -0700

    Update layoutfull.php

--- layoutfull.php
+++ layoutfull.php
@@ -1,19 +1,30 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>CT310 Examples</title>
-		<?php echo Asset::css('style.css'); ?>
-	</head>
-	<body>
-		<div id="head">
-			<h1>South Carolina</h1>
-		</div>
-		<div id="mainContent">
-			<?=$content; ?>
-		</div>
-		<div id="footer">
-			Part of a <a href="https://www.cs.colostate.edu/~ct310/">CT310</a> Course Project
-		</div>
-	</body>
+        <head>
+                <meta charset="utf-8">
+                <title>South Carolina</title>
+                <?php echo Asset::css('style.css'); ?>
+        </head>
+        <body>
+                <div id="header">
+                        <h1>South Carolina</h1>
+                </div> 
+                <nav class="navigation">
+                        <ul>
+                                <li><a href="<?=Uri::create('index.php/sc/index'); ?>">S</a></li>
+                                <li><a href="<?=Uri::create('index.php/sc/index'); ?>">U</a></li>
+                                <li><a href="<?=Uri::create('index.php/sc/index'); ?>">C</a></li>
+                                <li><a href="<?=Uri::create('index.php/sc/index'); ?>">K</a></li>
+                                <li><a href="<?=Uri::create('index.php/sc/index'); ?>">S</a></li>
+                        </ul>   
+                </nav>
+                <!--$content is called by the controller located in fuel/app/classes/controller-->
+                <div id="body">
+                        <?=$content; ?>
+                </div>
+
+                <div id="footer">
+                        This site is part of a <a href="https://www.cs.colostate.edu/~ct310/">CT310</a> Course Project
+                </div>
+        </body>
 </html>
