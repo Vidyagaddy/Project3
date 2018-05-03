3ffbfc89c85d6ac6c7046bf9d38cdb4fdf7b5a59
Vidyagaddy <31290797+Vidyagaddy@users.noreply.github.com>
2018-05-03 15:27:41 -0600

    Add files via upload

--- allstatus.php
+++ allstatus.php
@@ -4,6 +4,86 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
+
+    $.ajax("/~ct310/yr2018sp/master.json", {
+        success: function(data){
+         
+            $.each(data,function(e){
+                $.ajax("http://www.cs.colostate.edu/~"+data[e].eid+"/ct310/index.php/federation/status", {
+                    success: function(status,textStatus){
+                        var rt = "";
+                        var table = document.getElementById('allstatus');
+                        var j = table.rows.length;
+                        rt = "<tr>";
+                        rt += "<td>"+data[e].nameShort +"</td>";
+                        rt += "<td>"+data[e].nameLong+"</td>";
+                        if(textStatus != 'success'){
+                            rt += "<td style= 'color: yellow;'>"+unknown+"</td>";
+                        }
+                        if(typeof(status) == 'string'){
+                            var jstat = JSON.parse(status);
+                            if(jstat.status == 'open'){
+                            rt += "<td style='color:green'>"+jstat.status+"</td>";
+                            }
+                            else{
+                            rt += "<td style='color:red'>"+jstat.status+"</td>";
+                            }
+                        }
+                        else if(typeof(status) == 'object'){
+                            if(status.status == 'open'){
+                            rt += "<td style='color:green'>"+status.status+"</td>";
+                            }
+                            else{
+                            rt += "<td style='color:red'>"+status.status+"</td>"; 
+                            }
+                        }
+                        else{
+                            rt += "<td style= 'color: yellow;'>"+unknown+"</td>";
+                        }
+                        rt += "</tr>";
+                        var rr = table.insertRow(j);
+                        rr.innerHTML = rt; 
+                    }
+                },{
+                error: function(){
+                    var rt = "";
+                        var table = document.getElementById('allstatus');
+                        var j = table.rows.length;
+                        rt = "<tr>";
+                        rt += "<td>"+data[e].nameShort +"</td>";
+                        rt += "<td>"+data[e].nameLong+"</td>";
+                        rt += "<td style= 'color: yellow;'>"+unknown+"</td>";
+                        rt += "</tr>";
+                        var rr = table.insertRow(j);
+                        rr.innerHTML = rt;
+                        
+                }
+                });
+             
+            });
+        }
+            
+    }); 
+});
+    /*var statusArray = new Array();
+    $.when($.ajax("/~ct310/yr2018sp/master.json",{
+        success: function(data){
+            var len = data.length;
+            for(j=len-1; j >= 0; j--){
+                $.Deferred.then($.ajax("http://www.cs.colostate.edu/~"+data[j].eid+"/ct310/index.php/federation/status",{
+                success:function(d){
+                    if(typeof(d) == 'string'){
+                        var jd = JSON.parse(d);
+                        statusArray.push(jd.status);
+                    }
+                    else{
+                        statusArray.push(d.status);
+                    }
+                }
+                }));
+            }
+        }
+    }));
     $.ajax("/~ct310/yr2018sp/master.json",{
         success: function(data){
             var len = data.length;
@@ -14,27 +94,7 @@ $(document).ready(function(){
                 rt = "<tr>";
                 rt += "<td>"+data[j].nameShort +"</td>";
                 rt += "<td>"+data[j].nameLong+"</td>";
-               
-            $.ajax("http://www.cs.colostate.edu/~"+data[j].eid+"/ct310/index.php/federation/status",{
-                success:function(d){
-                 
-                    if(typeof(d) == 'string'){
-                        var jdata = JSON.parse(d);
-                        rt += "<td>"+jdata.status+"</td>";
-                        rt += "</tr>";
-                var rr = table.insertRow(i);
-                rr.innerHTML = rt; 
-                    }
-                    else{
-                        rt+="<td>"+d.status+"</td>";
-                        rt += "</tr>";
-                var rr = table.insertRow(i);
-                rr.innerHTML = rt; 
-                    }
-                }
-                
-            });
-            
+                rt += "<td>"+statusArray[j]+"</td>";
                rt += "</tr>";
                 var rr = table.insertRow(i);
                 rr.innerHTML = rt; 
@@ -60,7 +120,7 @@ function addRow(data,status){
     var rr = table.insertRow(i);
     rr.innerHTML = rt;
 }
-
+*/
 </script>
 </head>
 <body>
