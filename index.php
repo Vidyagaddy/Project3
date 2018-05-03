63281290b6bf1411b4c7b0b57e9eaabed17286a9
Vidyagaddy <31290797+Vidyagaddy@users.noreply.github.com>
2018-05-03 16:32:37 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -19,9 +19,7 @@
         <?php foreach($attrs as $attr){?>
     
         <span style ="margin-left: 50%">
-		<a href="<?=Uri::create('index.php/sc/view/'.$attr['attrID']); ?>">
-             <?php echo Asset::img($attr['image'],array("width" => "500px"));?>
-             <figcaption style="margin-left: 50%"><?php echo $attr['title'];?></figcaption>
+		<a href="<?=Uri::create('index.php/sc/view/'.$attr['attrID']); ?>"><?php echo $attr['title'];?>
 		</a>
         </span>
        
@@ -29,3 +27,48 @@
         </div>
         
         </div>
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+
+    $.ajax("/~ct310/yr2018sp/master.json", {
+        success: function(data){
+         
+            $.each(data,function(e){
+                $.ajax("http://www.cs.colostate.edu/~"+data[e].eid+"/ct310/index.php/federation/status", {
+                    success: function(status,textStatus){
+                       if(status.status == "open"){
+                            $.ajax("http://www.cs.colostate.edu/~"+data[e].eid+"/ct310/index.php/federation/listing", {
+                            success: function(lists){
+                                $.each(lists,function(i){
+                                    var li = "";
+                                    var list = document.getElementById('allattrs');
+                                    li = "<li>"+lists[i].name+"</li>";
+                                    var rr = list.
+                                })
+                            }
+                       }
+                    }
+                });
+             
+            });
+        }
+            
+    }); 
+});
+
+</script>
+</head>
+<body>
+<table id = "allattrs" style="border: 1px solid black;">
+<tr>
+<th>Short Name </th><th>Long Name </th><th>Status </th>
+</tr>
+</table>
+<ul id = "allattrs">
+</ul>
+</body>
+</html>
