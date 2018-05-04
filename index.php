736aa18fc2aa6647fd85f4c313c9df0d7fe1ae14
Vidyagaddy <31290797+Vidyagaddy@users.noreply.github.com>
2018-05-04 15:11:47 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
 
+    $.ajax("/~ct310/yr2018sp/master.json", {
+        success: function(data){
+         
+            $.each(data,function(e){
+            	
+                $.ajax("http://www.cs.colostate.edu/~"+data[e].eid+"/ct310/index.php/federation/status", {
+                    success: function(status){
+                       if(status.status == "open"){
+                            $.ajax("http://www.cs.colostate.edu/~"+data[e].eid+"/ct310/index.php/federation/listing", {
+                           
+                            
+                            success: function(lists){
+                            	
+                                $.each(lists,function(i){
+                                    var rt = "";
+                                    var table = document.getElementById('allattrs');
+                                    var j = table.rows.length;
+                                    rt = "<tr>";
+                                    rt += "<td>";
+                                    var php = '<?=Uri::create('index.php/sc/view');?>';
+                                    var str = lists[i].name;
+                                    rt+= str.link(php+"/"+data[e].eid+"/"+lists[i].id);
+                                    rt+="</td>";
+                                    rt+="</tr>";
+                                    var rr = table.insertRow(j);
+                                    rr.innerHTML = rt;
+                                });
+                               
+                            }
+                       });
+                    }
+                }
+                });
+            });
+        }
+            
+    }); 
+});
+
+</script>
 <h2> Welcome </h2>
-<div class="body">
-
+</head>
+<body>
         <p>All important attractions found here: </p>
         <?php 
         $adminCheck = false;
@@ -15,60 +61,14 @@
         if($adminCheck == false){?>
         <a href='<?=Uri::create('index.php/sc/brochure');?>'>+ Get Brochure </a>
         <?php } ?>
+        <br>
         <div id=attractions>
-        <?php foreach($attrs as $attr){?>
-    
-        <span style ="margin-left: 50%">
-		<a href="<?=Uri::create('index.php/sc/view/'.$attr['attrID']); ?>"><?php echo $attr['title'];?>
-		</a>
-        </span>
-       
-        <?php } ?>
+        <br>
+        <table id = "allattrs" style="border: 1px solid black;">
+        <tr>
+        <th>All Attractions</th>
+        </tr>
+        </table>
         </div>
-        
-        </div>
-<!DOCTYPE html>
-<html>
-<head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-
-    $.ajax("/~ct310/yr2018sp/master.json", {
-        success: function(data){
-         
-            $.each(data,function(e){
-                $.ajax("http://www.cs.colostate.edu/~"+data[e].eid+"/ct310/index.php/federation/status", {
-                    success: function(status,textStatus){
-                       if(status.status == "open"){
-                            $.ajax("http://www.cs.colostate.edu/~"+data[e].eid+"/ct310/index.php/federation/listing", {
-                            success: function(lists){
-                                $.each(lists,function(i){
-                                    var li = "";
-                                    var list = document.getElementById('allattrs');
-                                    li = "<li>"+lists[i].name+"</li>";
-                                    var rr = list.
-                                })
-                            }
-                       }
-                    }
-                });
-             
-            });
-        }
-            
-    }); 
-});
-
-</script>
-</head>
-<body>
-<table id = "allattrs" style="border: 1px solid black;">
-<tr>
-<th>Short Name </th><th>Long Name </th><th>Status </th>
-</tr>
-</table>
-<ul id = "allattrs">
-</ul>
 </body>
 </html>
--- view.php
+++ view.php
@@ -7,28 +7,24 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
-	$.ajax("http://www.cs.colostate.edu/~"+$eid+"/ct310/index.php/federation/attraction/"+$id, {
+	$.ajax("http://www.cs.colostate.edu/~"+"<?=$eid?>"+"/ct310/index.php/federation/attraction/"+<?=$id?>, {
     success: function(attr){
-    	document.getElementById('name').innerHTML = attr.name;
+    	 document.getElementById('title').innerHTML = attr.name; 
          document.getElementById('desc').innerHTML = attr.desc;       
     }
     });
-    $.ajax("http://www.cs.colostate.edu/~"+$eid+"/ct310/index.php/federation/attrimage/"+$id, {
-    success: function(image){
-         document.getElementById('image').innerHTML = image;       
-    }
-    });
-}
+    
+});
 </script>	
 </h2>
-	<h6> Title </h6>
-		<p  id = 'name'></p>
-	<h5> Description </h5>
+	<p id="title"></p>
 	<p id="desc"></p>
 	<br>
 	<br>
-	<div id="image">
-	</div>
+	<p id="image">
+		<img src = "http://www.cs.colostate.edu/~<?=$eid?>/ct310/index.php/federation/attrimage/<?=$id?>"></img>
+		
+	</p>
 
 	<br>
 	
